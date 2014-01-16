8b9dfc4c419cbdefdba63d21ebf1b79f82c561b4
pierre <pierre.nespo@gmail.com>
2014-01-16 11:58:58 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+:warning: *Use of this software is subject to important terms and conditions as set forth in the License file* :warning:
+
+# Show Original Email App
+
+## Description
+Display links to the original comment emails.
+
+## App Location
+
+* Ticket sidebar
+
+## Set-up/installation instructions
+
+Nothing special, you only need to install the app.
+
+## Demo
+
+![Demo gif](http://cl.ly/image/3J1q2247141Y/show_email_app.gif)
+
+## Contributions
+
+Pull requests are welcome.
---
+++ app.js
@@ -0,0 +1,53 @@
+(function() {
+
+  return {
+    defaultState: 'loading',
+    requests: {
+      fetchAudits: function() {
+        return {
+          url: helpers.fmt('/api/v2/tickets/%@/audits.json?include=users',
+                           this.ticket().id())
+        };
+      }
+    },
+    events: {
+      'app.activated'           : 'onAppActivated',
+      'fetchAudits.done'        : 'onFetchAuditsDone'
+    },
+
+    onAppActivated: function(app) {
+      if (app.firstLoad) {
+        this.ajax('fetchAudits');
+      }
+    },
+
+    onFetchAuditsDone: function(data) {
+      var comments = _.reduce(data.audits, function(memo, audit) {
+        if (audit.via.channel === 'email') {
+          var comment = _.first(_.filter(audit.events, function(e){
+            return e.type === 'Comment';
+          }));
+
+          if (comment) {
+            var author = _.first(_.filter(data.users, function(u) {
+              return u.id == comment.author_id;
+            }));
+
+            comment.audit_id = audit.id;
+            comment.author = author;
+            comment.created_at = new Date(audit.created_at).toLocaleString();
+
+            memo.push(comment);
+          }
+        }
+
+        return memo;
+      }, []);
+
+      this.switchTo('main', { comments: comments });
+
+      this.$('a').tooltip({ placement: 'left', html: true });
+    }
+  };
+
+}());
